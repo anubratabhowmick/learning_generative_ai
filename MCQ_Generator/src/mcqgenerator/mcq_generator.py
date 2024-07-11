85b586cda8bccfd45645e43919ad32589b78c018
Anubrata Bhowmick <anubratabhowmick@gmail.com>
2024-07-12 00:00:31 +0200

    Updated generator

--- MCQ_Generator/src/mcqgenerator/mcq_generator.py
+++ MCQ_Generator/src/mcqgenerator/mcq_generator.py
@@ -1,18 +1,11 @@
-import json
-import traceback
-import pandas as pd
-
 from langchain.prompts import PromptTemplate
 from langchain.chains import LLMChain, SequentialChain
 
 from langchain_openai import ChatOpenAI
 
-from langchain_community.llms import OpenAI
-from langchain_community.callbacks.manager import get_openai_callback
 
 from src.mcqgenerator.logger import logging
 from src.mcqgenerator.template import TEMPLATE1, TEMPLATE2
-from src.mcqgenerator.utils import read_file, get_table_data
 
 from dotenv import load_dotenv
 load_dotenv()
